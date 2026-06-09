65d4f75e35482bc74ca52a2f14c4a8a5e4243a82
Lucas <160195375+Lucas-jorg@users.noreply.github.com>
2026-06-09 16:01:24 -0300

    validacao.js

--- validacao.js
+++ validacao.js
@@ -1,1 +1,32 @@
+const form = document.getElementById("meuForm");
 
+form.addEventListener("submit", function(event){
+
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const idade = document.getElementById("idade").value;
+
+    const erro = document.getElementById("erro");
+
+    erro.textContent = "";
+
+    if(nome.trim() === ""){
+        event.preventDefault();
+        erro.textContent = "Nome obrigatório";
+        return;
+    }
+
+    if(!email.includes("@")){
+        event.preventDefault();
+        erro.textContent = "Email inválido";
+        return;
+    }
+
+    if(idade <= 0){
+        event.preventDefault();
+        erro.textContent = "Idade deve ser maior que zero";
+        return;
+    }
+
+    erro.textContent = "Formulário válido!";
+});
