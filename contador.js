5e062bb466f3889a04d8074a1b98e40905e2d848
Lucas <160195375+Lucas-jorg@users.noreply.github.com>
2026-06-09 16:02:29 -0300

    contador.js

--- contador.js
+++ contador.js
@@ -1,1 +1,35 @@
+let contador = 0;
 
+const numero = document.getElementById("numero");
+
+function atualizar(){
+    numero.textContent = contador;
+
+    if(contador > 0){
+        numero.style.color = "green";
+    }
+    else if(contador < 0){
+        numero.style.color = "red";
+    }
+    else{
+        numero.style.color = "black";
+    }
+}
+
+document.getElementById("incrementar")
+.addEventListener("click", function(){
+    contador++;
+    atualizar();
+});
+
+document.getElementById("decrementar")
+.addEventListener("click", function(){
+    contador--;
+    atualizar();
+});
+
+document.getElementById("zerar")
+.addEventListener("click", function(){
+    contador = 0;
+    atualizar();
+});
